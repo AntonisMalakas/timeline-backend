25c9396b44324fc1fa75ba8ed45efd9a68a55eb3
Q503700 <anthony.el-haddad@bmw.de>
2020-10-22 12:34:09 +0200

    add path to index.html

--- app.js
+++ app.js
@@ -46,7 +46,14 @@ require('./config/passport');
 
 app.use(require('./routes'));
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+// app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+
 /// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
